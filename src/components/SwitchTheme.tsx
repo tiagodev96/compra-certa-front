201809d917cefa9ed5ed39dac0de3602e72e0906
Tiago Batista <tiago.dev96@gmail.com>
2024-02-28 22:32:49 -0300

    refactor: theme switcher update

--- src/components/SwitchTheme.tsx
+++ src/components/SwitchTheme.tsx
@@ -1,24 +1,42 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Switch } from "./ui/switch";
-import { useThemeStore } from "@/store/ThemeStore";
 import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+import { Skeleton } from "./ui/skeleton";
 
 export const SwitchTheme = () => {
-  const [isDark, toggleTheme] = useThemeStore((state) => [
-    state.isDark,
-    state.toggle,
-  ]);
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const handleCheckChange = (checked: boolean) => {
+    setTheme(checked ? "dark" : "light");
+  };
+
+  if (!mounted)
+    return (
+      <div className="flex flex-row space-x-2 items-center">
+        <Skeleton className="w-8 h-8 rounded-full" />
+        <Skeleton className="w-14 h-6 rounded-lg" />
+      </div>
+    );
 
   return (
     <div className="flex flex-row space-x-2 items-center">
-      {isDark ? (
+      {resolvedTheme === "dark" ? (
         <MoonIcon className="text-neutral-50 w-[22px] h-[22px]" />
       ) : (
         <SunIcon className="text-neutral-950 w-[22px] h-[22px]" />
       )}
-      <Switch onCheckedChange={toggleTheme} checked={isDark} />
+      <Switch
+        onCheckedChange={handleCheckChange}
+        checked={resolvedTheme === "dark"}
+      />
     </div>
   );
 };
