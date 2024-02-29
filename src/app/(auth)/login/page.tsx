7a10cbbceb20cffc31e598ee67310252cbd3f130
Tiago Batista <tiago.dev96@gmail.com>
2024-02-28 22:32:59 -0300

    feat: login changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,13 +4,13 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="sectionContainer flex items-center justify-center bg-neutral-50 w-screen h-screen">
+    <div className="sectionContainer flex items-center justify-center w-screen h-screen">
       <div className="grid grid-cols-2 max-w-[1200px] container h-full items-center justify-center max-h-[600px] rounded-lg drop-shadow-2xl">
         <div className="bg-neutral-200 w-full h-full rounded-tl-lg rounded-bl-lg"></div>
 
-        <div className="bg-neutral-950 h-full overflow-hidden w-full rounded-tr-lg rounded-br-lg px-8 py-8">
+        <div className=" h-full overflow-hidden w-full rounded-tr-lg rounded-br-lg">
           <GridBackground>
-            <div className="flex flex-1 flex-col z-20">
+            <div className="flex flex-1 flex-col z-20 px-8 py-8">
               <LoginHeader />
               <Separator className="mt-8 bg-neutral-200" />
             </div>
--- src/components/Auth/LoginHeader.tsx
+++ src/components/Auth/LoginHeader.tsx
@@ -6,7 +6,7 @@ import { SwitchTheme } from "..";
 export const LoginHeader = () => {
   return (
     <div className="flex flex-row justify-between items-center">
-      <Image width={100} height={91.6} src="/logo-dark.svg" alt="logo" />
+      <Image width={90} height={82.44} src="/logo-dark.svg" alt="logo" />
       <SwitchTheme />
     </div>
   );
